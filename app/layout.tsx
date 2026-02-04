1a57adacb0116cc4c7a8a472f55ff44f37a13183
Gyeongsu <140276820+infiniste@users.noreply.github.com>
2026-02-04 15:18:16 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,20 @@ import "./globals.css";
 
 const SITE_NAME = "빠른 정보 웹";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="DL8u4t5f4WlydJSIW2RUrqM-j4Ie2w8N3I3Y2kahTDU"
+        />
+        <title>{SITE_NAME}</title>
+      </head>
       <body>
         <div className="header">
           <div className="container">
@@ -31,4 +42,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
-
